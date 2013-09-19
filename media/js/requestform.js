c6d905bd54a7898b73943d1d5b886096e4ea35e9
thefewminutes <mark@produkt.us>
2013-09-19 13:35:24 -0700

    change layout, hide/display content
    moved elements in upload form to streamline usability, added classes to
    hide upload form (phone, tablet) and display message (phone, tablet)

--- media/js/requestform.js
+++ media/js/requestform.js
@@ -1,5 +1,6 @@
 $(function() {
-	$("#RevDate").datepicker(); // datepicker for revision date
+	// datepicker for revision date
+	$("#RevDate").datepicker();
 			
 	// date builder for grabbing current date
 	var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
@@ -8,15 +9,13 @@ $(function() {
 	var newDate = new Date();
 	newDate.setDate(newDate.getDate() + 1);    
 	$('#submitDate').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
-	// end date builder
 	
 	// pretty file upload
 	$('input[id=lefile]').change(function() {
 		$('#photoCover').val($(this).val());
 	});
-	//end file upload
 	
 	//popovers
-	$('#approvalPopover').popover({trigger: 'hover'});
-	$('#deptApprovalPopover').popover({trigger: 'hover'});
+	$('#approvalPopover').popover({trigger: 'hover', placement: 'auto right'});
+	$('#deptApprovalPopover').popover({trigger: 'hover', placement: 'auto right'});
 });
