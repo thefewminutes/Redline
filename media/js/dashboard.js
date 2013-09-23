1a5b1e58cbdd146e75e49ebfe1141cd900d412df
Mark Johnson <mark@produkt.us>
2013-09-23 11:01:39 -0700

    add truncate text
    add truncate text jquery to dashboard in-progress table descriptions.
    add table sorter jquery plugin

--- media/js/dashboard.js
+++ media/js/dashboard.js
@@ -1,14 +1,28 @@
-$('#inprogress a').click(function (e) {
-	e.preventDefault()
-	$(this).tab('show')
-})
+$.expander.defaults.slicePoint = 60; // expander default value
 
-$('#pending a').click(function (e) {
-	e.preventDefault()
-	$(this).tab('show')
-})
+$(document).ready(function() {
+	$('#inprogress a').click(function (e) {
+		e.preventDefault()
+		$(this).tab('show')
+	})
 
-$('#closed a').click(function (e) {
-	e.preventDefault()
-	$(this).tab('show')
-})
+	$('#pending a').click(function (e) {
+		e.preventDefault()
+		$(this).tab('show')
+	})
+
+	$('#closed a').click(function (e) {
+		e.preventDefault()
+		$(this).tab('show')
+	})
+	
+	// Expander https://github.com/kswedberg/jquery-expander
+	$('tbody.expandable tr td').expander({
+		slicePoint: 80,
+		expandPrefix: ' ',
+		expandText: '[...]',
+		collapseTimer: 0,
+		userCollapseText: '[^]',
+		preserveWords: true
+	});
+});
