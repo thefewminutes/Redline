28e30420332b6d27ac721bd4386982a6c43ff9b4
Mark Johnson <mark@produkt.us>
2013-10-23 15:58:11 -0700

    create separate factories for each controller

--- media/js/redlines.js
+++ media/js/redlines.js
@@ -4,32 +4,66 @@ redlineApp.config(function($routeProvider) {
 	$routeProvider
 		.when('/',
 			{
-				controller: 'redlinesController',
+				controller: 'inprogressController',
 				templateUrl: 'partials/inprogress.html'
 			})
 		.when('/pending',
 			{
-				controller: 'redlinesController',
+				controller: 'pendingController',
 				templateUrl: 'partials/pending.html'
 			})
 		.when('/closed',
 			{
-				controller: 'redlinesController',
+				controller: 'closedController',
 				templateUrl: 'partials/closed.html'
 			})
 		.otherwise({ redirectTo: '/'});
 });
 //$locationProvider.html5Mode(true);
 
-// get all the redlines
-redlineApp.factory('redlineFactory', function($http) {
+// get all the in progress redlines
+redlineApp.factory('inprogressFactory', function($http) {
 	var redlines = {content:null};
 	$http.get('media/json/redlines.json')
 		.success(function(data) {
 			redlines.content = data;
 	})
 		.error(function(data) {
-			// handle get errors
+			alert('could not get data');
+	});
+	var factory = {};
+	factory.getRedlines = function() {
+		return redlines;
+	};
+	return factory;
+});
+
+// get all the pending redlines
+redlineApp.factory('pendingFactory', function($http) {
+	var redlines = {content:null};
+	$http.get('media/json/redlines.json')
+		.success(function(data) {
+			redlines.content = data;
+	})
+		.error(function(data) {
+			alert('could not get data');
+	});
+	var factory = {};
+	factory.getRedlines = function() {
+		return redlines;
+	};
+	return factory;
+});
+
+// get all the closed redlines
+redlineApp.factory('closedFactory', function($http) {
+	var redlines = {content:null};
+	$http.get('media/json/redlines.json')
+		.success(function(data) {
+			redlines.content = data;
+	})
+		.error(function(data) {
+			alert('could not get data');
 	});
 	var factory = {};
 	factory.getRedlines = function() {
@@ -40,17 +74,50 @@ redlineApp.factory('redlineFactory', function($http) {
 
 // create controllers
 var controllers = {};
-//define first controller
-controllers.redlinesController = function ($scope, redlineFactory) {
+
+// inprogress tab controller
+controllers.inprogressController = function ($scope, inprogressFactory) {
 	$scope.redlines = [];
 	init();
 	function init() {
-		$scope.redlines = redlineFactory.getRedlines();
+		$scope.redlines = inprogressFactory.getRedlines();
 	}
 	// delete redline
 	$scope.deleteRedline = function (index) {
-    $scope.redlines.content.redlines.splice(index, 1)
-  };
+    	//$scope.redlines.content.redlines.splice(index, 1)
+		alert(index);
+	};
+
+};
+
+// pending tab controller
+controllers.pendingController = function ($scope, pendingFactory) {
+	$scope.redlines = [];
+	init();
+	function init() {
+		$scope.redlines = pendingFactory.getRedlines();
+	}
+	// delete redline
+	$scope.deleteRedline = function (index) {
+    	//$scope.redlines.content.redlines.splice(index, 1)
+		alert(index);
+	};
+
+};
+
+// closed tab controller
+controllers.closedController = function ($scope, closedFactory) {
+	$scope.redlines = [];
+	init();
+	function init() {
+		$scope.redlines = closedFactory.getRedlines();
+	}
+	// delete redline
+	$scope.deleteRedline = function (index) {
+    	//$scope.redlines.content.redlines.splice(index, 1)
+		alert(index);
+	};
+
 };
 
 redlineApp.controller(controllers);
